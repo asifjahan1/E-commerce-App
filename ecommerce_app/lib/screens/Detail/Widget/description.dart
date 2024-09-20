7b71ccf7258e5737d7acdefd857c62ecfef270ee
Asif Jahan <asifjahan307@gmail.com>
2024-09-21 00:40:12 +0600

    required modifications (#30)

--- ecommerce_app/lib/screens/Detail/Widget/description.dart
+++ ecommerce_app/lib/screens/Detail/Widget/description.dart
@@ -1,10 +1,17 @@
 import 'package:ecommerce_app/constants.dart';
 import 'package:flutter/material.dart';
 
-class Description extends StatelessWidget {
+class Description extends StatefulWidget {
   final String description;
 
-  const Description({Key? key, required this.description}) : super(key: key);
+  const Description({super.key, required this.description});
+
+  @override
+  _DescriptionState createState() => _DescriptionState();
+}
+
+class _DescriptionState extends State<Description> {
+  String selectedTab = "Description";
 
   @override
   Widget build(BuildContext context) {
@@ -12,52 +19,95 @@ class Description extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Container(
-              width: 120,
-              height: 40,
-              decoration: BoxDecoration(
-                color: kprimaryColor,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              alignment: Alignment.center,
-              child: const Text(
-                "Description",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  fontSize: 16,
-                ),
-              ),
+            _buildTabButton(
+              title: "Description",
+              isSelected: selectedTab == "Description",
+              onPressed: () {
+                setState(() {
+                  selectedTab = "Description";
+                });
+              },
             ),
-            Text(
-              "Specifications",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-                fontSize: 16,
-              ),
+            _buildTabButton(
+              title: "Specifications",
+              isSelected: selectedTab == "Specifications",
+              onPressed: () {
+                setState(() {
+                  selectedTab = "Specifications";
+                });
+              },
             ),
-            Text(
-              "Reviews",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-                fontSize: 16,
-              ),
+            _buildTabButton(
+              title: "Reviews",
+              isSelected: selectedTab == "Reviews",
+              onPressed: () {
+                setState(() {
+                  selectedTab = "Reviews";
+                });
+              },
             ),
           ],
         ),
         const SizedBox(height: 20),
-        Text(
-          description,
+        _buildTabContent(),
+      ],
+    );
+  }
+
+  // Widget to build each tab button
+  Widget _buildTabButton(
+      {required String title,
+      required bool isSelected,
+      required VoidCallback onPressed}) {
+    return Expanded(
+      child: Container(
+        height: 40,
+        decoration: BoxDecoration(
+          color: isSelected ? kprimaryColor : Colors.transparent,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: TextButton(
+          onPressed: onPressed,
+          child: Text(
+            title,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: isSelected ? Colors.white : Colors.black,
+              fontSize: 16,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTabContent() {
+    switch (selectedTab) {
+      case "Specifications":
+        return const Text(
+          "Here are the product specifications...",
+          style: TextStyle(
+            fontSize: 16,
+            color: Colors.grey,
+          ),
+        );
+      case "Reviews":
+        return const Text(
+          "Here are the product reviews...",
+          style: TextStyle(
+            fontSize: 16,
+            color: Colors.grey,
+          ),
+        );
+      default:
+        return Text(
+          widget.description,
           style: const TextStyle(
             fontSize: 16,
             color: Colors.grey,
           ),
-        ),
-      ],
-    );
+        );
+    }
   }
 }
