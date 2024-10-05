ac5558aae7b4bbbde3b8523e0dfa28b374dc9fd4
Asif Jahan <asifjahan307@gmail.com>
2024-10-05 23:33:27 +0600

    added Category items (#49)

---
+++ ecommerce_app/lib/screens/Category/category_items.dart
@@ -0,0 +1,18 @@
+import 'package:ecommerce_app/constants.dart';
+import 'package:flutter/material.dart';
+
+class Category extends StatefulWidget {
+  const Category({super.key});
+
+  @override
+  State<Category> createState() => _CategoryState();
+}
+
+class _CategoryState extends State<Category> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: kcontentColor,
+    );
+  }
+}
