b689eb23e20e3cd9826211a26a190512892cc80a
Asif Jahan <asifjahan307@gmail.com>
2024-11-19 15:58:19 +0600

    added category section (#71)

--- ecommerce_app/lib/screens/Category/category_items.dart
+++ ecommerce_app/lib/screens/Category/category_items.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_app/constants.dart';
+import 'package:ecommerce_app/models/category.dart';
 import 'package:flutter/material.dart';
 
 class Category extends StatefulWidget {
@@ -21,6 +22,51 @@ class _CategoryState extends State<Category> {
         ),
         centerTitle: true,
         automaticallyImplyLeading: false,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          shrinkWrap: true,
+          physics: const AlwaysScrollableScrollPhysics(),
+          itemCount: categoriesList.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 8,
+            crossAxisSpacing: 8,
+            childAspectRatio: 1.0,
+          ),
+          itemBuilder: (context, index) {
+            final category = categoriesList[index];
+            return Card(
+              color: kprimaryColor,
+              shadowColor: kcontentColor,
+              elevation: 10,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    category.image,
+                    width: 60,
+                    height: 60,
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    category.title,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                     ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
