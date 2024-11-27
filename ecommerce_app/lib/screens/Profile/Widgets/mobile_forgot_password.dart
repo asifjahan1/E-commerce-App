1fe731926fd0d72b05f883e0e8d9977e1e2b6ca2
Asif Jahan <asifjahan307@gmail.com>
2024-11-28 00:57:32 +0600

    responsive the forget password screen (#86)

--- ecommerce_app/lib/screens/Profile/Widgets/mobile_forgot_password.dart
+++ ecommerce_app/lib/screens/Profile/Widgets/mobile_forgot_password.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: unused_local_variable
 
+import 'package:ecommerce_app/responsive.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce_app/constants.dart';
 import 'package:ecommerce_app/screens/Profile/Widgets/register_mobile.dart';
@@ -98,84 +99,252 @@ class _ForgotPasswordState extends State<ForgotPassword> {
           height: double.infinity,
           color: Colors.white,
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextFormField(
-                  controller: _phoneNumberController,
-                  decoration: InputDecoration(
-                    hintText: "Enter Mobile Number",
-                    helperText: "e.g: +8801234567890",
-                    helperStyle: const TextStyle(
-                      color: Colors.grey,
-                    ),
-                    border: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                        color: Colors.grey,
-                        width: 1.5,
-                      ),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    filled: true,
-                    fillColor: Colors.white,
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter your phone number';
-                    } else if (!_isValidPhoneNumber(value)) {
-                      return 'Invalid phone number format';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 20),
-                TextFormField(
-                  controller: _passwordController,
-                  decoration: InputDecoration(
-                    hintText: "Make a New Password",
-                    border: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                        color: Colors.grey,
-                        width: 1.5,
-                      ),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    filled: true,
-                    fillColor: Colors.white,
-                  ),
-                  obscureText: true,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter a new password';
-                    } else if (!_isValidPassword(value)) {
-                      return 'Password must be at least 6 characters';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(height: 20),
-                MaterialButton(
-                  onPressed: _resetPassword,
-                  color: kprimaryColor,
-                  textColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 40,
-                    vertical: 16,
-                  ),
-                  child: const Text(
-                    "Confirm",
-                    style: TextStyle(fontSize: 16),
-                  ),
-                ),
-              ],
-            ),
+          child: Responsive(
+            mobile: _buildMobileView(),
+            tablet: _buildTabletView(),
+            desktop: _buildDesktopView(),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildMobileView() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextFormField(
+            controller: _phoneNumberController,
+            decoration: InputDecoration(
+              hintText: "Enter Mobile Number",
+              helperText: "e.g: +8801234567890",
+              helperStyle: const TextStyle(
+                color: Colors.grey,
+              ),
+              border: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  color: Colors.grey,
+                  width: 1.5,
+                ),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              filled: true,
+              fillColor: Colors.white,
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter your phone number';
+              } else if (!_isValidPhoneNumber(value)) {
+                return 'Invalid phone number format';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 20),
+          TextFormField(
+            controller: _passwordController,
+            decoration: InputDecoration(
+              hintText: "Make a New Password",
+              border: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  color: Colors.grey,
+                  width: 1.5,
+                ),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              filled: true,
+              fillColor: Colors.white,
+            ),
+            obscureText: true,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter a new password';
+              } else if (!_isValidPassword(value)) {
+                return 'Password must be at least 6 characters';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 20),
+          MaterialButton(
+            onPressed: _resetPassword,
+            color: kprimaryColor,
+            textColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            padding: const EdgeInsets.symmetric(
+              horizontal: 40,
+              vertical: 16,
+            ),
+            child: const Text(
+              "Confirm",
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTabletView() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextFormField(
+            controller: _phoneNumberController,
+            decoration: InputDecoration(
+              hintText: "Enter Mobile Number",
+              helperText: "e.g: +8801234567890",
+              helperStyle: const TextStyle(
+                color: Colors.grey,
+              ),
+              border: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  color: Colors.grey,
+                  width: 1.5,
+                ),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              filled: true,
+              fillColor: Colors.white,
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter your phone number';
+              } else if (!_isValidPhoneNumber(value)) {
+                return 'Invalid phone number format';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 20),
+          TextFormField(
+            controller: _passwordController,
+            decoration: InputDecoration(
+              hintText: "Make a New Password",
+              border: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  color: Colors.grey,
+                  width: 1.5,
+                ),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              filled: true,
+              fillColor: Colors.white,
+            ),
+            obscureText: true,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter a new password';
+              } else if (!_isValidPassword(value)) {
+                return 'Password must be at least 6 characters';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 30),
+          MaterialButton(
+            onPressed: _resetPassword,
+            color: kprimaryColor,
+            textColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            padding: const EdgeInsets.symmetric(
+              horizontal: 50,
+              vertical: 16,
+            ),
+            child: const Text(
+              "Confirm",
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDesktopView() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextFormField(
+            controller: _phoneNumberController,
+            decoration: InputDecoration(
+              hintText: "Enter Mobile Number",
+              helperText: "e.g: +8801234567890",
+              helperStyle: const TextStyle(
+                color: Colors.grey,
+              ),
+              border: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  color: Colors.grey,
+                  width: 1.5,
+                ),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              filled: true,
+              fillColor: Colors.white,
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter your phone number';
+              } else if (!_isValidPhoneNumber(value)) {
+                return 'Invalid phone number format';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 20),
+          TextFormField(
+            controller: _passwordController,
+            decoration: InputDecoration(
+              hintText: "Make a New Password",
+              border: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  color: Colors.grey,
+                  width: 1.5,
+                ),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              filled: true,
+              fillColor: Colors.white,
+            ),
+            obscureText: true,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter a new password';
+              } else if (!_isValidPassword(value)) {
+                return 'Password must be at least 6 characters';
+              }
+              return null;
+            },
+          ),
+          const SizedBox(height: 40),
+          MaterialButton(
+            onPressed: _resetPassword,
+            color: kprimaryColor,
+            textColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            padding: const EdgeInsets.symmetric(
+              horizontal: 60,
+              vertical: 18,
+            ),
+            child: const Text(
+              "Confirm",
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+        ],
       ),
     );
   }
